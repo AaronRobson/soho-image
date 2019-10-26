7388c44042ab6decde04c2edd6ce4605cf216c81
Aaron Robson <arobsonse@hotmail.co.uk>
2019-10-26 23:10:09 +0100

    build in travis and circleci

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # soho-image
+
+[![Build Status](https://travis-ci.org/AaronRobson/soho-image.svg?branch=master)](https://travis-ci.org/AaronRobson/soho-image)
+[![CircleCI](https://circleci.com/gh/AaronRobson/soho-image.svg?style=svg)](https://circleci.com/gh/AaronRobson/soho-image)
+[![Coverage Status](https://coveralls.io/repos/github/AaronRobson/soho-image/badge.svg?branch=master)](https://coveralls.io/github/AaronRobson/soho-image?branch=master)
+
 Find and display pictures of the Sun from NASA's Solar and Heliospheric Observatory (SOHO) spacecraft.
--- soho_image.py
+++ soho_image.py
@@ -7,47 +7,56 @@ import os.path
 on the command-line and attempts to open them in the default web browser.
 
 To-do:
-use urllib etc to join up url properly rather than relying on similarities with local filepaths.
+use urllib etc to join up url properly rather than relying on similarities
+with local filepaths.
 '''
 
 rootSite = 'https://sohowww.nascom.nasa.gov/'
 
+
 def ImagePathMask(animated, small):
-	if animated:
-		pathMask = 'data/LATEST/current_%%s%s.gif' % ('small'*small)
-	else:
-		pathMask = 'data/realtime/%%s/%d/latest.jpg' % ((1024, 512)[small])
+    if animated:
+        pathMask = 'data/LATEST/current_%%s%s.gif' % ('small'*small)
+    else:
+        pathMask = 'data/realtime/%%s/%d/latest.jpg' % ((1024, 512)[small])
 
-	return os.path.join(rootSite, pathMask)
+    return os.path.join(rootSite, pathMask)
 
-#these aren't lined up: 'mdi_igr', 'mdi_mag', 'c2', 'c3'
+
+# These aren't lined up: 'mdi_igr', 'mdi_mag', 'c2', 'c3'.
 imageTypes = ('eit_171', 'eit_195', 'eit_284', 'eit_304')
 
+
 def ImageAddresses(animated, small):
-	return (ImagePathMask(animated, small) % (type) for type in imageTypes)
+    return (ImagePathMask(animated, small) % (type) for type in imageTypes)
+
 
 aboutPath = os.path.join(rootSite, 'data/realtime/image-description.html')
 
+
 def OpenWebpage(url):
-	'''Open a webpage and print addresses and status to screen.
-	'''
-	try:
-		webbrowser.open(url)
-	except webbrowser.Error:
-		prefix =  'Could not open'
-	else:
-		prefix = 'Opened'
-	
-	return '%s: "%s"' % (prefix, url)
+    '''Open a webpage and print addresses and status to screen.
+    '''
+    try:
+        webbrowser.open(url)
+    except webbrowser.Error:
+        prefix = 'Could not open'
+    else:
+        prefix = 'Opened'
+
+    return '%s: "%s"' % (prefix, url)
+
 
 def Main(animated=True, small=True):
-	print('Please see: "%s".\n' % (aboutPath))
-	print('Settings:\n\tAnimated: %s\n\tSmall: %s\n' % (animated, small))
+    print('Please see: "%s".\n' % (aboutPath))
+    print('Settings:\n\tAnimated: %s\n\tSmall: %s\n' % (animated, small))
 
-	for message in (OpenWebpage(imageAddress) for imageAddress in ImageAddresses(animated, small)):
-		print(message)
+    for message in (OpenWebpage(imageAddress)
+                    for imageAddress in ImageAddresses(animated, small)):
+        print(message)
+
 
 if __name__ == "__main__":
-	Main()
+    Main()
 
-	input('\nPress Enter to Exit:')
+    input('\nPress Enter to Exit:')
